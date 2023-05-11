6a983b1f16679abbe00b43c6d7fbe3fd29541912
lrag14 <laurentrober@gmail.com>
2023-05-11 22:41:51 +0200

    northflank

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 require("dotenv").config();
 const express = require("express");
+
 const cors = require("cors");
-const axios = require("axios"); //1
+
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
 
 app.get("/characters", async (req, res) => {
-	try {
-		const name = req.query.name || "";
-		const skip = req.query.skip || "0";
-		const limit = req.query.limit || "100";
+  try {
+    const name = req.query.name || "";
+    const skip = req.query.skip || "0";
+    const limit = req.query.limit || "100";
 
-		const response = await axios.get(
-			//1******Requet du Front******
-			// console.log(response.data);
-			res.json(response.data)
-		); // 2***reponse au front
-		// *********************ENVOI AU FRONT
-	} catch (error) {
-		res.status(500).json({message: error.message});
-	}
+    // **************$INTEROGE LES SERVEUR
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&name=${name}&skip=${skip}&limit=${limit}`
+    );
+    // *******REPONSE du data au front
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.all("*", (req, res) => {
-	res.status(404).json({message: "This route does not exist"});
+  res.status(404).json({ message: "Bad Way" });
 });
 
 app.listen(process.env.PORT, () => {
-	console.log("Server OP");
+  console.log("Server OP");
 });
